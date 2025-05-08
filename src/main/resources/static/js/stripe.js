73f7df9a828ac3036f2a6d0b7285bcc3645fb976
tana-shi <shingo_tana626@icloud.com>
2025-05-09 00:04:34 +0900

    Delete src directory

--- src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51RGefHFkxK3vzEHmYBUNDcawhWgTwxpbsZWHRLgMIflvqlES5FKpry0ahlzzAPTUYqUoGOwqVUwcGd5WAAhzcHID00ZRCYrxUi');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
- stripe.redirectToCheckout({
-   sessionId: sessionId
- })
-});
